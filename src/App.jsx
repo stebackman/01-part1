e393113db5791fb85635b85ac578d922dd1de2ff
stebackman <stebackman@gmail.com>
2024-08-16 11:08:38 +0300

    tehty muutama tehtävä lisää. Tehty komponentti hello yms

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-const App = () => {
-  console.log("Hello from komponentti");
+const Hello = (props) => {
   return (
     <div>
-      <p>Komponentissa sisätö: Hello world</p>
+      <p>Hello {props.name}, tämä on hellokomponentti</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log(now, a + b);
+  console.log("Tämä on tilanne komponentissa nyt:");
+
+  return (
+    <div>
+      <Hello name="Stefan" />
+      <Hello name="Vadim" />
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   );
 };
